8176a335271ac0f272f91eb9a3e0901741c51dc7
D3cl4n <dpmurphy@wpi..edu>
2023-03-08 22:58:42 -0500

    progress

--- tmto.py
+++ tmto.py
@@ -6,7 +6,7 @@ class TMTO:
     def __init__(self, t):
         self.t = t
         self.modulus = 2^self.t
-        self.cnt = {2 : 99, 3 : 999, 4 : 9999}
+        self.cnt = {2 : 2**16, 3 : 999, 4 : 9999}
         self.chains = {}
 
     def decimal_to_bin(self, n):
@@ -20,6 +20,17 @@ class TMTO:
     def truncate(self, ciphertext, n):
         return ciphertext[:32 - n]
 
+    def recompute_chain(self, SP, sha_hash):
+        print("[+] Re-computing the chain now")
+        tmp = SP
+        previous = ""
+        while True:
+            ciphertext = self.truncate(hashlib.sha256(str(tmp).encode("utf-8")).hexdigest(), 16)
+            if ciphertext == sha_hash:
+                print("[+] We hit an endpoint")
+                return tmp #this is the previous input to SHA256 and the plaintext
+            tmp = ciphertext
+
     def compute_chains(self):
         starting_point = random.randint(0, self.cnt[self.t / 8])
         chain_sp = self.truncate(hashlib.sha256(str(starting_point).encode("utf-8")).hexdigest(), 16)
@@ -27,25 +38,29 @@ class TMTO:
 
         counter = 1
         tmp = chain_sp
-        while counter <= self.cnt[self.t / 8]:
+        while counter <= self.cnt[self.t / 8]: #99 since we truncate SHA256 hash to 16 bits
             ciphertext = self.truncate(hashlib.sha256(str(tmp).encode("utf-8")).hexdigest(), 16)
             tmp = ciphertext
-            if counter == cnt[self.t/8]:
+            if counter == self.cnt[self.t/8]:
                 print(f"[+] Finished {counter} SHA256 calculations")
                 print("[+] Reached end of chain calculation, storing SP and EP")
                 chain_ep = tmp
                 self.add_SP_EP(chain_sp, chain_ep)
             counter += 1
 
-    def find_preimage(self, hash):
-        random_start = random.randint(0, self.cnt[self.t / 8])
-        ciphertext = self.truncate(hashlib.sha256(str(random_start).encode("utf-8")).hexdigest(), 16)
-        tmp = ciphertext
+    def find_preimage(self, sha_hash):
+        print("[+] Now attempting to find pre-image")
+        tmp = sha_hash
         counter = 0
-        while counter < self.cnt[self.t/8]:
+        print(self.cnt[self.t/8])
+        while counter <= self.cnt[self.t/8]:
             tmp = self.truncate(hashlib.sha256(str(tmp).encode("utf-8")).hexdigest(), 16)
             if self.chains[2] == tmp:
-                print("[+] ")
+                print(f"[+] Found endpoint {tmp} re-computing chain now")
+                plaintext = self.recompute_chain(self.chains[1], sha_hash)
+                print(f"[+] Recovered plaintext {plaintext} for hash {sha_hash} verifying...")
+
+            counter += 1
 
 '''
 Description: Driver code for the attack
